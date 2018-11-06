c72edc391631eae6d3e629f222d9f32ce26747a6
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-11-06 22:00:11 +0530

    [Styles] fixed and added responsive styles

--- js/main.js
+++ js/main.js
@@ -163,23 +163,27 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const restInfoContainer = document.createElement('div');
+  restInfoContainer.classList = 'restaurant-info-container';
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  restInfoContainer.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  restInfoContainer.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  restInfoContainer.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  restInfoContainer.append(more)
 
+  li.append(restInfoContainer); 
   return li
 }
 
