7168128d2032b3563e7b2804a583686e1c7cb005
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-11-15 13:10:57 +0530

    [Fix] Images on the homepage have alt attributes

--- js/main.js
+++ js/main.js
@@ -158,6 +158,7 @@ createRestaurantHTML = restaurant => {
 
 	const image = document.createElement('img');
 	image.className = 'restaurant-img';
+	image.alt = restaurant.alt;
 	image.src = DBHelper.imageUrlForRestaurant(restaurant);
 	li.append(image);
 
